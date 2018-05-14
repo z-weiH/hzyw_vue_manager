11b82bcadf7461552b6aeca376afb776b3dbe016
zengwei <18279047775@163.com>
2018-05-14 15:46:13 +0800

    增加文件导出， 证据模板设置

--- src/views/finance/contractAddNewDefault/index.vue
+++ src/views/finance/contractAddNewDefault/index.vue
@@ -1,12 +1,26 @@
 <template>
   <div class="contract-add-new-default">
     合同加款【财务人员】
+    <el-button @click="handleExport">导出</el-button>
   </div>
 </template>
 
 <script>
+  import exportFile from '@/assets/js/exportFile'
   export default {
-    
+    methods : {
+      handleExport() {
+        exportFile({
+          url : 'http://118.31.39.65:8888/contractOrder/exportExcel.htm',
+          data : {
+            keyWords : '',
+            startTime : '',
+            endTime : '',
+            orderStatus : '',
+          },
+        });
+      },
+    },
   }
 </script>
 
