7507ac782f46d553ee71cc74749f286ae28100a5
wanhuanchang <1109923832@qq.com>
2018-05-30 17:38:22 +0800

    accountconfirm

--- src/views/account/accountAffirm/index.vue
+++ src/views/account/accountAffirm/index.vue
@@ -16,17 +16,19 @@
             <template slot-scope="scope" >
               <el-button
                 size="mini"
-                @click="" v-if="scope.row.orderStatus == 0" >待提交</el-button>
-              <el-button
-                size="mini"
-                @click="" v-if="scope.row.orderStatus == 1" >待审核</el-button>
-              <span v-if="scope.row.orderStatus == 2">通过</span>
-              <span v-if="scope.row.orderStatus == 3">不通过</span>
+                @click="showDailg(scope.row)" v-if="scope.row.custStatus == 30" >待确认</el-button>
+              <span v-if="scope.row.custStatus == 10">待提交</span>
+              <span v-if="scope.row.custStatus == 11">待审核</span>
+              <span v-if="scope.row.custStatus == 20">待开户设置</span>
+              <span v-if="scope.row.custStatus == 40">开户成功</span>
+              <span v-if="scope.row.custStatus == 41">开户失败</span>
             </template>
           </el-table-column>
         </table-component>
       </div>
     </div>
+    <edit :edit-state="editState" :item="item" ></edit>
+    <confirm :edit-state="editState" :item="item"  @refresh="refresh"></confirm>
   </div>
 </template>
 
@@ -35,27 +37,32 @@ import Mixins from '@/components/script/_mixin'
 import Searchs from '@/components/searchs'
 import TableComponent from '@/components/table'
 import {URL_JSON} from "../../../components/script/url_json";
+import edit from './modules/edit'
+import confirm from './modules/confirm'
 export default {
   name: 'accountAffirm',
   extends: Mixins,
   data () {
     return {
+      item: {},
       searchItems : [
         {type: 'text',placeholder: '请输入企业名称、账户', property: 'keyWords', colSpan: 7},
         {type: 'date',placeholder: '请输入开始时间', property: 'startTime', colSpan: 5},
         {type: 'date',placeholder: '请输入结束时间', property: 'endTime', colSpan: 5},
-        // 0待提交1待审核2通过3不通过
+        //<!--10待提交11 待审核20待开户设置30待确认40开户成功41开户失败-->
         {type: 'select',placeholder: '开户状态', property: 'orderStatus', colSpan: 4,options:[
             {label:'全部', value: ''},
-            {label:'待提交', value: 0},
-            {label:'待审核', value: 1},
-            {label:'通过', value: 2},
-            {label:'不通过', value: 3},
+            {label:'待提交', value: 10},
+            {label:'待审核', value: 11},
+            {label:'待开户设置', value: 20},
+            {label:'待确认', value: 30},
+            {label:'开户成功', value: 40},
+            {label:'开户失败', value: 41},
           ]},
       ],
       columnDefine: [
         {label: '企业名称',property: 'custName'},
-        {label: '合同号',property: 'contactNo'},
+        {label: '合同号',property: 'contactNo',isLink: 1, linkShowPanel: this.doView},
         {label: '技术服务费 （元）',property: 'serveAmount'},
         {label: '开户仲券（张）',property: 'ticketCount'},
         {label: '开户受理费（元）',property: 'caseAmount'},
@@ -65,9 +72,31 @@ export default {
       searchItem: {}
     }
   },
+  methods: {
+    doView(row) {
+      this.$http.post(URL_JSON['editAccountAffirm'],{orderId: row.orderId})
+        .then(res => {
+          if(res.code == '0000'){
+            this.item = res.result;
+            this.editState = 9;
+          }
+        })
+    },
+    showDailg(row){
+      this.$http.post('/order/selectOpenCustomerConfirmAuditDetail.htm',{orderId: row.orderId})
+        .then(res => {
+          if(res.code == '0000'){
+            this.item = res.result;
+            this.editState = 2;
+          }
+        })
+    }
+  },
   components: {
     Searchs,
-    TableComponent
+    TableComponent,
+    edit,
+    confirm
   },
   created () {
     this.doQuery(this.queryUrl,this.searchItem);
---
+++ src/views/account/accountAffirm/modules/edit.vue
@@ -0,0 +1,185 @@
+<template>
+  <el-dialog
+    :visible.sync="show"
+    :title="title"
+    width="890px"
+    center>
+    <div class="dailog-container">
+      <table-edits :editDefines="edtDefines" :item="item" :disabled="true"></table-edits>
+      <table
+        class="m-primordial-table el-table el-table--fit el-table--border el-table--enable-row-hover"
+      >
+      <tr v-if="item.auditList && item.auditList.length > 0">
+        <td colspan="4" >第八部分：审核结果</td>
+      </tr>
+      </table>
+      <table-edits :editDefines="editDefines" :item="audit" v-for="(audit,index) in item.auditList" :key="index" :disabled="true"></table-edits>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+import TableEdits from '@/components/tableEdits'
+import {URL_JSON} from "../../../../components/script/url_json";
+export default {
+  name: 'edit',
+  props: {
+    editState: Number,
+    item: Object
+  },
+  data () {
+    return {
+      title:'开户设置',
+      edtDefines: [{
+        title: '第一部分：客户基本信息',
+        content: [
+          {label: '客户全称：', type: 'text', placeholder: '请输入客户全称',columns:1,property: 'custName'},
+          {label: '年营业额（万元）：', type: 'text', placeholder: '请输入年营业额',columns:1,property: 'busiAmount'},
+          {label: '社会唯一信用号：', type: 'text', placeholder: '请输入社会唯一信用号',columns:1,property: 'custIdcard'},
+          {label: '邮箱(账户)：', type: 'text', placeholder: '请输入邮箱(账户)',columns:1,property: 'loginName'},
+          {label: '企业注册地址：', type: 'text', placeholder: '请输入企业注册地址',columns:2,property: 'custIdaddress'},
+          {label: '网址或应用(名称)：', type: 'text', placeholder: '请输入网址或应用(名称)',columns:2,property: 'custWebsite'},
+        ]
+      },{
+        title: '第二部分：法定代表人信息',
+        content: [
+          {label: '法定代表人：', type: 'text', placeholder: '请输入法定代表人',columns:1,property: 'legallerName'},
+          {label: '身份证号：', type: 'text', placeholder: '请输入身份证号',columns:1,property: 'legallerIdcard'},
+          {label: '法定代表人手机：', type: 'text', placeholder: '请输入法定代表人手机',columns:1,property: 'legallerPhone'},
+          {label: '法定代表人邮箱：', type: 'text', placeholder: '请输入法定代表人邮箱',columns:1,property: 'legallerEmail'},
+          {label: '法定代表人职务：', type: 'text', placeholder: '请输入法定代表人职务',columns:2,property: 'legallerPosition'},
+          {type: 'info',columns:2,content:'注：法定代表人手机和邮箱将用于接收案件信息，请与客户确认'}
+        ]
+      },{
+        title: '第三部分：联系人信息',
+        content: [
+          {label: '客户联系人：', type: 'text', placeholder: '请输入客户联系人',columns:1,property: 'contactsName'},
+          {label: '联系电话：', type: 'text', placeholder: '请输入联系电话',columns:1,property: 'contactsPhone'},
+          {label: '联系邮箱：', type: 'text', placeholder: '请输入联系邮箱',columns:1,property: 'contactsEmail'},
+          {label: '微信或QQ：', type: 'text', placeholder: '请输入微信或QQ',columns:1,property: 'contactsQq'},
+        ]
+      },{
+        title: '第四部分：配置信息',
+        content: [
+          {label: '选择仲裁委：', type: 'select', options: this.AllArbList,columns:1,property: 'arbId',valuefield: 'arbId', labelfield: 'fullName'},
+          // 1主账户、2子账户
+          {label: '客户类型：', type: 'select', options: [{label: '主账户', value: 1},{label: '子账户', value: 2}],columns:1,property: 'custType'},
+          {label: '选择关联的普通客户：', type: 'select', options: [],columns:2,property: 'parentId', valuefield: 'custId', labelfield: 'custName',
+            hidden: () => this.item.custType === 2
+          },
+
+        ]
+      },{
+        title: '第五部分：客户资料',
+        content: [
+          {label: '营业执照(jpg，png)', type: 'file', placeholder: '请输入客户联系人',columns:1,property: 'dataUrl',disabledLabel: '点击查看营业执照', accept: 'image/jpeg,image/png'},
+          {type: 'img',columns:1,property:'dataUrl'},
+        ]
+      },{
+        title: '第六部分：合同信息',
+        content: [
+          {label: '合同编号：', type: 'text', placeholder: '请输入合同编号',columns:1,property: 'contractNo'},
+          {label: '合同时间：', type: 'date', placeholder: '请输入合同时间',columns:1,property: 'contractDate'},
+          {label: '预缴仲裁受理费（元）：', type: 'text', placeholder: '请输入预缴仲裁受理费',columns:1,property: 'preCaseAmt'},
+          {label: '技术服务费（元）：', type: 'text', placeholder: '请输入技术服务费',columns:1,property: 'serviceAmt'},
+          {label: '充值仲券（张）：', type: 'text', placeholder: '请输入充值仲券',columns:1,property: 'preCaseTicket'},
+          {label: '仲券金额（元）：', type: 'text', placeholder: '请输入仲券金额',columns:1,property: 'preTicketAmt'},
+          {label: '赠送仲券（张）：', type: 'text', placeholder: '请输入赠送仲券',columns:1,property: 'preGiftTicket'},
+          {label: '赠券有效期 ：', type: 'select', placeholder: '请选择赠券有效期',columns:1,property: 'preGiftPeriod',options: [
+              {label: '请选择赠券有效期', value: ''},
+              {label: '1个月', value: '1'},
+              {label: '2个月', value: '2'},
+              {label: '3个月', value: '3'},
+              {label: '4个月', value: '4'},
+              {label: '5个月', value: '5'},
+              {label: '6个月', value: '6'},
+              {label: '7个月', value: '7'},
+              {label: '8个月', value: '8'},
+              {label: '9个月', value: '9'},
+              {label: '10个月', value: '10'},
+              {label: '11个月', value: '11'},
+              {label: '12个月', value: '12'},
+            ],hidden: () => {
+              return this.item.preGiftTicket && this.item.preGiftTicket != 0;
+            }},
+        ]
+      },{
+        title: '第七部分：所属负责人',
+        content: [
+          {label: '市场人员：',  type: 'select', options: [{label: '杭州仲裁委员会', value: 'hz'},{label: '衢州仲裁委员会', value: 'qz'}],columns:1,placeholder:'请选择市场人员',property: 'marketerId',valuefield: 'userId', labelfield: 'userName'},
+          {type: 'info',columns:1, content:''},
+        ]
+      }],
+      editDefines: [
+        {
+          title: '',
+          content: [
+            {label: '操作人：',  type: 'text',columns:1,property: 'apprerName'},
+            {label: '操作时间：',  type: 'text',columns:1,property: 'audtiTime'},
+            {label: '审核状态描述：',  type: 'text',columns:1,property: 'apprerStatusDesc'},
+            {type: 'info',columns:1, content:''},
+            {label: '审核结果：',  type: 'textarea',columns:2,property: 'apprerResult'},
+          ]
+        }
+      ]
+    }
+  },
+  computed: {
+    show :{
+      get: function () {
+        return this.editState === 9;
+      },
+      set: function (v) {
+        if(!v)
+          this.$parent.editState = 0
+      }
+    }
+  },
+  components: {
+    TableEdits
+  },
+  methods: {
+
+  },
+  mounted () {
+  },
+  methods: {
+    getAllArbList() {
+      this.$http.post(URL_JSON['ArbListAccountApply'])
+        .then(res=> {
+          if(res.code === '0000'){
+            this.edtDefines[3].content[0].options = res.result;
+          }
+        })
+    },
+    getUserList() {
+      this.$http.post(URL_JSON['RoleTypeAccountApply'],{type: 'MARKETER'})
+        .then(res=> {
+          if(res.code === '0000'){
+            this.edtDefines[6].content[0].options = res.result;
+          }
+        })
+    },
+    getParentCustomer() {
+      this.$http.post(URL_JSON['getParentCustomerAccountApply'])
+        .then(res => {
+          if(res.code === '0000'){
+            this.edtDefines[3].content[2].options = res.result.list;
+          }
+        })
+    },
+  },
+  created () {
+    this.getAllArbList();
+    this.getUserList();
+    this.getParentCustomer();
+  }
+}
+</script>
+
+<style scoped>
+  .dailog-container{
+    height: 542px;
+    overflow-y: scroll;
+  }
+</style>
