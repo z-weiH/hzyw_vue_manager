42b478bbde1babe3e0a7b13680b1e8d26d670c45
zengwei <18279047775@163.com>
2018-06-04 18:49:30 +0800

    init

--- src/axios/host.js
+++ src/axios/host.js
@@ -7,8 +7,9 @@ let host = {
   // target: 'http://192.168.30.16:8999', // 徐炜
   // target: 'http://192.168.30.15:1234',   //陈博
   // target: 'http://192.168.30.18:8999',   //刘全全
-  target: 'http://118.31.39.65:7777',   // 测试环境
+  // target: 'http://118.31.39.65:7777',   // 测试环境
   // target: 'http://192.168.30.64:3000',   // 本地node
+  target: '',
 };
 
 export default host;
