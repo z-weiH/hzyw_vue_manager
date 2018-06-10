cab1b974568ea9ed97d9d41884dcb11eb3a5561e
zengwei <18279047775@163.com>
2018-06-10 17:23:55 +0800

    推送记录

--- src/store/modules/menu.js
+++ src/store/modules/menu.js
@@ -1,11 +1,29 @@
+import axios from 'axios'
+
 export default {
 	namespaced: true,
 	state : {
+		// 菜单 当前选中
 		menuActive : '',
+		// 推送记录 未读数量
+		pushRecordUnread : 0,
 	},
 	mutations : {
 		setMenuActive(state, data) {
 			state.menuActive = data;
+		},
+		setPushRecordUnread(state, data) {
+			state.pushRecordUnread = data;
+		},
+	},
+	actions : {
+		upDataPushRecordUnread(context) {
+			axios({
+				method : 'post',
+				url : '/pushRecord/queryUnReadMsgNum.htm',
+			}).then((res) => {
+				context.commit('setPushRecordUnread',res.result.unReadMsgNum);
+			});
 		},
 	},
 }
--- src/views/pretrial/messagePushList/index.vue
+++ src/views/pretrial/messagePushList/index.vue
@@ -1,15 +1,199 @@
 <template>
-  <div>
+  <div class="message-push-list">
+    <div class="wsbodyhead">
+      <a>所在位置</a>
+      <a href="javascript:;" class="aside_tit">推送记录</a>
+    </div>
 
+    <div class="item-search">
+      <el-form :inline="true" ref="ruleForm" :model="ruleForm">
+        <el-form-item label=" " prop="keyWords">
+          <el-input style="width:300px;" v-model.trim="ruleForm.keyWords" placeholder="请输入互金企业"></el-input>
+        </el-form-item>
+
+        <el-button @click="handleSearch" type="warning">查询</el-button>
+        <el-button @click="handleSign" type="info">标记全部为已读</el-button>
+      </el-form>
+    </div>
+
+    <div class="item-title">
+      推送记录列表
+    </div>
+
+    <div class="item-table">
+      <table
+        class="m-primordial-table mt-10
+          el-table el-table--fit el-table--border 
+          el-table--enable-row-hover"
+          style="table-layout:fixed;"
+      >
+        <colgroup>
+          <col style="width:50px;"></col>
+          <col></col>
+        </colgroup>
+        <tbody>
+          <template v-for="(item,index) in tableData">
+            <tr :Key="item.batchNo + '' + index">
+              <td>{{index + 1}}</td>
+              <td class="m-td">
+                <div class="fl">
+                  <i class="is-read"></i>
+                  <span class="mr-10 m-tit">{{item.merchantName}}</span>  
+                  <el-button type="primary" round size="mini">正在推送</el-button>
+                  <el-button type="info" round size="mini">处理完成</el-button>
+                  <el-button round size="mini">推送完成，正在处理</el-button>
+
+                  <p class="m-num">
+                    <span>处理总数：{{item.totalNumber}}</span>
+                    <span>处理失败：{{item.failCount}}</span>
+                  </p>
+                </div>
+                <div class="fr">
+                  <div class="fr-box">
+                    <div v-if="item.startTime">推送开始时间   {{item.startTime}}</div>
+                    <div v-if="item.endTime">推送完成时间   {{item.endTime}}</div>
+                    <div v-if="item.finishDisposeTime">处理完成时间   {{item.finishDisposeTime}}</div>
+                  </div>
+                </div>
+              </td>
+            </tr>
+          </template>
+        </tbody>
+      </table>
+      <!-- 分页 -->
+      <el-pagination
+        class="mt-10 mb-10"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="currentPage"
+        :page-sizes="[10, 20, 30, 40]"
+        :page-size="10"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total">
+      </el-pagination>
+
+    </div>
   </div>
 </template>
 
 <script>
   export default {
-    
+    data() {
+      return {
+        ruleForm : {
+          // 互金企业
+          keyWords : '',
+        },
+
+        // 表格数据
+        tableData : [{fullName : '张三'}],
+        // 数据总数
+        total : 11,
+        // 当前页数
+        currentPage : 1,
+        // 每页数量
+        pageSize : 10,
+      }
+    },
+    mounted() {
+      this.initTableList();
+    },
+    methods : {
+      // 点击搜索
+      handleSearch() {
+        this.currentPage = 1;
+        this.initTableList();
+      },
+      // 点击标记 
+      handleSign() {
+        this.$http({
+          method : 'post',
+          url : '/pushRecord/setReadedFlag.htm',
+        }).then((res) => {
+          this.currentPage = 1;
+          this.initTableList();
+        });
+      },
+
+      // 表格相关 start
+
+      // 初始化 表格数据
+      initTableList() {
+        this.$http({
+          method : 'post',
+          url : '/pushRecord/queryListByCondition.htm',
+          data : {
+            pageSize : this.pageSize,
+            currentNum : this.currentPage,
+            endDate : this.ruleForm.endDate,
+            startDate : this.ruleForm.startDate,
+            keyWords : this.ruleForm.keyWords,
+            orderStatus : this.ruleForm.orderStatus,
+          },
+        }).then((res) => {
+          this.total = res.result.count;
+          this.tableData = res.result.list;
+        });
+      },
+      // 页数 change
+      handleSizeChange(val) {
+        this.pageSize = val;
+        this.currentPage = 1;
+        this.initTableList();
+      },
+      // 分页 change
+      handleCurrentChange(val) {
+        this.currentPage = val; 
+        this.initTableList();
+      },
+
+      // 表格相关 end
+    },
   }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+
+.message-push-list{
+  table td{
+    width: 100px;
+  }
+  .is-read{
+    display: inline-block;
+    width: 10px;
+    height: 10px;
+    background-color: rgb(241, 194, 107);
+    border-radius: 50%;
+    margin-right: 5px;
+  }
+  .m-tit{
+    font-size: 15px;
+    font-weight: bold;
+  }
+  .m-td{
+    text-align: left;
+    padding-left: 10px!important;
+    .fl{
+      width: 60%;
+    }
+  }
+  .fr-box{
+    text-align: right;
+    padding-right: 20px;
+    padding-top: 30px;
+    >div{
+      margin-bottom: 10px;
+    }
+  }
+
+  .m-num{
+    margin-top: 10px;
+    padding-left: 20px;
+    span{
+      float: left;
+      width: 50%;
+    }
+  }
+}
 
 </style>
