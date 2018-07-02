41e71dd29d2a1feeacd558f877631d5b1d18a8a7
zengwei <18279047775@163.com>
2018-07-02 16:23:46 +0800

    cdn2

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -66,7 +66,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     // see https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
       filename: config.build.index,
-      template: 'index.html',
+      template: 'index.prod.html',
       inject: true,
       minify: {
         removeComments: true,
