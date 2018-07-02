14f6810d254eb854251afd83e4a9199b2ed1c229
zengwei <18279047775@163.com>
2018-07-02 10:21:04 +0800

    1

--- src/router/jurisdictionExclude.js
+++ src/router/jurisdictionExclude.js
@@ -5,6 +5,7 @@ export default [
   '/main/hearDropCaseDetail', //证据缺失案件库 详情
   '/main/redoHearDetail', //案件复审 大批次
   '/redoHearChildDetail', //案件复审 小批次
+  '/main/initialHearDetail', // 案件初审 审核页面
   '/idCardHearDetail', //案件初审 身份证审核
   '/signatureHearDetail', //案件初审 签名审核
   '/evidenceWireHear', //案件初审 证据链审核
