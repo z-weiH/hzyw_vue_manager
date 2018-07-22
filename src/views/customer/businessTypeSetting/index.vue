7e86575bcb5bdec60d77d782b90121411c2f4723
wanhuanchang <1109923832@qq.com>
2018-07-22 13:33:56 +0800

    。。。

--- src/views/customer/businessTypeSetting/index.vue
+++ src/views/customer/businessTypeSetting/index.vue
@@ -8,12 +8,43 @@
       <span class="item-title-sign">数据类型</span>
     </div>
     <div class="item-table">
-      <table-component :noPager="true" :noSerial="true"  :pager="pager" :table-data="tableData" :column-define="columnDefine"></table-component>
+      <table-component :noPager="true" :noSerial="true"  :pager="pager" :table-data="tableData" :column-define="columnDefine">
+        <el-table-column label="启用状态" prop="orderStatusName" slot="defineCol" >
+          <template slot-scope="scope">
+            // { label: "启用状态", property: "enabledStatus",},
+            // 0:未启用,1:启用
+           <span v-if="scope.row.enabledStatus === 0" class="colLink">启用</span>
+           <span v-if="scope.row.enabledStatus === 1" class="colLink" style="color:#848484">停用</span>
+          </template>
+        </el-table-column>
+
+      </table-component>
+      <div class="center mt-20 mb-20">
+        <el-button type="primary" @click="HandleCreate">新增业务</el-button>
+      </div>
     </div>
+
+    <el-dialog
+      ref="dialog"
+      :visible.sync="showDailog"
+      :close-on-click-modal="false"
+      :close-on-press-escape="false"
+      title="新增业务"
+      width="640px"
+      center>
+      <div style="margin: 0 100px;">
+        <edits class="item-edits" formname="changePwd" ref="edits" :editItems="editItems" :item="item"></edits>
+      </div>
+      <span slot="footer" class="dialog-footer">
+          <el-button type="primary"   @click="saveHandle">保 存</el-button>
+          <el-button @click=" showDailog= 0" >取 消</el-button>
+        </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
+  import Edits from '@/components/edits'
   import Mixins from "@/components/script/_mixin";
   import TableComponent from "@/components/table";
   export default {
@@ -25,13 +56,31 @@
           { label: "业务类型", property: "busiType" },
           { label: "创建时间", property: "createTime" },
           { label: "产品数量", property: "productNum" },
-          { label: "启用状态", property: "enabledStatus" },
+
         ],
-        queryUrl: "",
+        //控制弹窗
+        showDailog: false,
+        item: {},
+        editItems: [
+          {label: '业务类型', property: 'loginName', type : 'text',placeholder:'请输入业务类型'},
+        ],
+        queryUrl: "/5/biz/queryBizSetTypeByBaseQuery.htm",
       }
     },
     components:{
-      TableComponent
+      TableComponent,
+      Edits
+    },
+    methods:{
+      HandleCreate() {
+        this.showDailog = true;
+      },
+      saveHandle() {
+
+      }
+    },
+    mounted() {
+      this.doQuery(this.queryUrl,{});
     }
   }
 </script>
--- src/views/customer/templateList/index.vue
+++ src/views/customer/templateList/index.vue
@@ -1,19 +1,81 @@
 <template>
   <div class="template-list">
+    <div class="wsbodyhead">
+      <a>所在位置</a>
+      <a href="javascript:;" class="aside_tit">模板列表</a>
+    </div>
+    <div class="item-search">
+      <searchs :search-items="searchItems" :item="searchItem" :queryUrl="queryUrl">
+        <div class="fr" slot="moreBtn">
+          <el-button type="primary" @click="create">新增仲裁委</el-button>
+        </div>
+      </searchs>
+    </div>
+
+    <div class="item-title of-hidden">
+      <span class="item-title-sign">客户列表</span>
+    </div>
+    <div class="item-table">
+      <table-component  :pager="pager" :table-data="tableData" :column-define="columnDefine">
+      </table-component>
+
+    </div>
 
   </div>
 </template>
 
 <script>
+  import Searchs from "@/components/searchs";
+  import Mixins from "@/components/script/_mixin";
+  import TableComponent from "@/components/table";
   export default {
-    
+    name:'templateList',
+    mixins: [Mixins],
+    data(){
+      return {
+        queryUrl:'',
+        searchItems: [
+          {
+            type: "text",
+            placeholder: "请输入客户名称",
+            property: "keyWords",
+            colSpan: 6
+          },
+          {type: 'date',placeholder: '预计完成日期', property: 'startDate', colSpan: 4, lt: 'endDate'},
+          {type: 'date',placeholder: '预计完成日期', property: 'endDate', colSpan: 4, gt: 'startDate'},
+        ],
+        searchItem: {},
+        columnDefine: [
+          { label: "客户", property: "clientName" },
+          { label: "产品", property: "productName" },
+          { label: "模板", property: "templateCode" },
+          { label: "启用状态", property: "templateStatus" },
+          { label: "申请书", property: "applyStatus" },
+          { label: "裁决书", property: "adjudeStatus" },
+          { label: "强制执行书", property: "enforceStatus" },
+          { label: "初次启用日期", property: "firstTime" },
+          { label: "备注", property: "remark" },
+        ]
+      }
+    },
+    components:{
+      TableComponent,
+      Searchs
+    },
+    methods: {
+
+    },
+    mounted(){
+
+    }
+
   }
 </script>
 
 <style lang="scss" scoped>
 
 .template-list{
-  
+
 }
 
 </style>
