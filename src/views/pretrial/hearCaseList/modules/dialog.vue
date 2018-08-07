63f3b863dd743f66a274f767773467711fea340c
zengwei <18279047775@163.com>
2018-08-07 11:41:12 +0800

    互金企业 or 模板接口，字段 修改

--- src/views/pretrial/hearCaseList/modules/dialog.vue
+++ src/views/pretrial/hearCaseList/modules/dialog.vue
@@ -43,9 +43,9 @@
             >
               <el-option
                 v-for="(item,index) in productOptions"
-                :key="item.prodCode + '' + index "
-                :label="item.prodName"
-                :value="item.prodCode">
+                :key="item.productId + '' + index "
+                :label="`${item.prodTempName} (${item.num})`"
+                :value="item.productId">
               </el-option>
             </el-select>
           </el-form-item>
@@ -305,9 +305,9 @@
         // 根据互金企业 查询 产品
         this.$http({
           method : 'post',
-          url : '/merchant/selectMerchantProductByMerchantCode.htm',
+          url : '/preCaseLib/queryProductTemplateListByClientCode.htm',
           data : {
-            merchantCode : val,
+            clientCode : val,
           },
         }).then((res) => {
           this.productOptions = res.result;
