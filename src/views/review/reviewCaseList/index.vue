46b9ae6a0e845b4f3174fc2efe0465b087a3d098
zengwei <18279047775@163.com>
2018-08-09 16:31:29 +0800

    复核管理 案件列表

--- src/views/review/reviewCaseList/index.vue
+++ src/views/review/reviewCaseList/index.vue
@@ -1,15 +1,255 @@
 <template>
-  <div>
+  <div class="review-case-list">
+    <div class="wsbodyhead">
+      <a>所在位置</a>
+      <a href="javascript:;" class="aside_tit">预审案件库</a>
+    </div>
+    <div class="item-search">
+      <el-form :inline="true" ref="ruleForm" :model="ruleForm" label-width="0px">
+        <el-form-item label=" " prop="keyWords">
+          <el-input style="width:400px;" v-model.trim="ruleForm.keyWords" placeholder="请输入互金企业、产品名称、模板号、被申请人姓名、手机"></el-input>
+        </el-form-item>
 
+        <el-form-item label=" " prop="accountPeriodType">
+          <el-select clearable style="width:120px;" v-model="ruleForm.accountPeriodType" placeholder="请选择账龄">
+            <el-option label="1~30天" value="M0"></el-option>
+            <el-option label="31~60天" value="M1"></el-option>
+            <el-option label="61~90天" value="M2"></el-option>
+            <el-option label="91~120天" value="M3"></el-option>
+            <el-option label="121~150天" value="M4"></el-option>
+            <el-option label="151~180天" value="M5"></el-option>
+            <el-option label="181~210天" value="M6"></el-option>
+            <el-option label="211~240天" value="M7"></el-option>
+            <el-option label="241~270天" value="M8"></el-option>
+            <el-option label="271~300天" value="M9"></el-option>
+            <el-option label="301~330天" value="M10"></el-option>
+            <el-option label="331~360天" value="M11"></el-option>
+            <el-option label="360天以上" value="M12"></el-option>
+          </el-select>
+        </el-form-item>
+
+        <!-- 推送时间 -->
+        <timeFrame
+          :startDate.sync="ruleForm.pushStartDate"
+          :endDate.sync="ruleForm.pushEndDate"
+          startPlaceholder="推送开始"
+          endPlaceholder="推送结束"
+        >
+        </timeFrame>
+
+        <br />
+
+        <!-- 预审时间 -->
+        <timeFrame
+          :startDate.sync="ruleForm.queryStartDate"
+          :endDate.sync="ruleForm.queryEndDate"
+          startPlaceholder="预审开始"
+          endPlaceholder="预审结束"
+          class="ml-10"
+        >
+        </timeFrame>
+
+        <el-form-item label=" " prop="status">
+          <el-select clearable style="width:150px;" v-model="ruleForm.status" placeholder="请选择状态">
+            <el-option label="待分配" :value="0"></el-option>
+            <el-option label="待初审" :value="1"></el-option>
+            <el-option label="待复审" :value="2"></el-option>
+            <el-option label="退回重审" :value="3"></el-option>
+            <el-option label="审核通过" :value="4"></el-option>
+            <el-option label="审核未通过" :value="5"></el-option>
+            <el-option label="立案申请成功" :value="6"></el-option>
+            <el-option label="立案申请失败" :value="7"></el-option>
+          </el-select>
+        </el-form-item>
+
+        <el-button @click="handleSearch" type="warning">查询</el-button>
+
+      </el-form>
+    </div>
+
+    <div class="item-title of-hidden">
+      <span class="item-title-sign">案件列表</span>
+      <div class="fr">
+        <el-button @click="handleDistributionCases" type="primary">分配案件</el-button>
+      </div>
+    </div>
+
+    <div class="item-table">
+      <el-table
+        :data="tableData"
+        border
+      >
+        <el-table-column prop="date" label="序号" width="50px">
+          <template slot-scope="scope">
+            {{scope.$index + 1}}
+          </template>
+        </el-table-column>
+        <el-table-column prop="platName" label="互金企业" width="150px" align="center">
+          <template slot-scope="scope">
+            <el-tooltip :content="scope.row.platName" placement="top-start">
+              <span class="ellipsis" style="max-width:129px;">{{scope.row.platName}}</span>
+            </el-tooltip>
+          </template>
+        </el-table-column>
+        <el-table-column prop="productName" label="产品模板" width="150px">
+          <template slot-scope="scope">
+            <el-tooltip :content="scope.row.productName" placement="top-start">
+              <span class="ellipsis" style="max-width:129px;">{{scope.row.productName}}</span>
+            </el-tooltip>
+          </template>
+        </el-table-column>
+        <el-table-column prop="respondents" label="被申请人" width="150px"></el-table-column>
+        <el-table-column prop="resPhone" label="被申请人手机" width="150px"></el-table-column>
+        <el-table-column prop="resPhone" label="申请立案时间" width="150px"></el-table-column>
+        <el-table-column prop="resPhone" label="初审人" width="150px"></el-table-column>
+        <el-table-column prop="resPhone" label="复审人" width="150px"></el-table-column>
+        <el-table-column align="center" prop="status" label="状态" width="150px">
+          <template slot-scope="scope">
+            {{
+              scope.row.status === 0 ? '待分配' :
+              scope.row.status === 1 ? '待初审' :
+              scope.row.status === 2 ? '待复审' :
+              scope.row.status === 3 ? '退回重审' :
+              scope.row.status === 4 ? '审核通过' :
+              scope.row.status === 5 ? '审核未通过' :
+              scope.row.status === 6 ? '立案申请成功' :
+              scope.row.status === 7 ? '立案申请失败' : ''
+            }}
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- 分页 -->
+      <el-pagination
+        class="mt-10 mb-10"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="currentPage"
+        :page-sizes="[10, 20, 30, 40]"
+        :page-size="10"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total">
+      </el-pagination>
+
+      <!-- 分配案件 dialog -->
+      <CaseDialog @successCBK="successCBK" ref="CaseDialog"></CaseDialog>
+    </div>
   </div>
 </template>
 
 <script>
+  import timeFrame from '@/components/timeFrame.vue'
+  import CaseDialog from './modules/dialog.vue'
   export default {
-    
+    components : {
+      timeFrame,
+      CaseDialog,
+    },
+    data() {
+      return {
+        ruleForm : {
+          // 互金企业
+          keyWords : '',
+          // 账龄
+          accountPeriodType : '',
+          // 推送开始时间
+          pushStartDate : '',
+          // 推送结束时间
+          pushEndDate	 : '',
+          // 预审完成开始查询日期
+          queryStartDate : '',
+          // 预审完成结束查询日期
+          queryEndDate : '',
+          // 状态 0:待分配,1:待初审,2:待复审,3:退回重审,4：审核通过,5：审核未通过,6:立案申请成功,7:立案申请失败
+          status : '',
+        },
+        rules : {},
+
+        // 表格数据
+        tableData : [],
+        // 数据总数
+        total : 0,
+        // 当前页数
+        currentPage : 1,
+        // 每页数量
+        pageSize : 10,
+
+      }
+    },
+    mounted() {
+      this.initTableList();
+    },
+    methods : {
+      // 点击搜索
+      handleSearch() {
+        this.currentPage = 1;
+        this.initTableList();
+      },
+      // 点击分配案件
+      handleDistributionCases() {
+        this.$refs.CaseDialog.show();
+      },
+      // 分配案件 成功回调
+      successCBK() {
+        this.currentPage = 1;
+        this.initTableList();
+      },
+
+      // 表格相关 start
+
+      // 初始化 表格数据
+      initTableList() {
+        this.$http({
+          url : '/preCaseLib/queryCaseListByCondition.htm',
+          method : 'post',
+          data : {
+            pageSize : this.pageSize,
+            currentNum : this.currentPage,
+            keyWords : this.ruleForm.keyWords,
+
+            accountPeriodType : this.ruleForm.accountPeriodType,
+            pushStartDate : this.ruleForm.pushStartDate,
+            pushEndDate : this.ruleForm.pushEndDate,
+            queryStartDate : this.ruleForm.queryStartDate,
+            queryEndDate : this.ruleForm.queryEndDate,
+            status : this.ruleForm.status,
+
+          },
+        }).then((res) => {
+          this.total = res.result.count;
+          res.result.list.forEach(it => {
+            it.resPhone = it.resPhone.substr(0,3) + "****" + it.resPhone.substr(7);
+          })
+          this.tableData = res.result.list;
+        });
+      },
+      // 页数 change
+      handleSizeChange(val) {
+        this.pageSize = val;
+        this.currentPage = 1;
+        this.initTableList();
+      },
+      // 分页 change
+      handleCurrentChange(val) {
+        this.currentPage = val;
+        this.initTableList();
+      },
+
+      // 表格相关 end
+    },
   }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+
+.review-case-list{
+  .item-title{
+    padding-top: 5px;
+    padding-bottom: 5px;
+  }
+  .item-title-sign{
+    margin-top: 12px;
+    display: inline-block;
+  }
+}
 
 </style>
---
+++ src/views/review/reviewCaseList/modules/dialog.vue
@@ -0,0 +1,453 @@
+<template>
+  <div class="review-case-list-dialog">
+    <el-dialog
+      title="分配案件"
+      :visible.sync="dialogVisible"
+      width="580px"
+      @close="handleClose"
+    >
+      <div class="m-conetnt">
+        <el-form ref="ruleForm" :model="ruleForm" :rules="rules" label-width="100px">
+          <div class="mb-5">
+            <span class="icon-sign">筛选条件</span>
+          </div>
+
+          <el-form-item label="互金企业" prop="merchantCode">
+            <el-select
+              style="width:100%"
+              v-model="ruleForm.merchantCode"
+              clearable
+              filterable
+              remote
+              reserve-keyword
+              placeholder="请输入关键词进行检索"
+              :remote-method="remoteMethod"
+              @change="handleMerchantCodeChange"
+            >
+              <el-option
+                v-for="(item,index) in merchantOptions"
+                :key="item.code + '' + index "
+                :label="item.merchantName"
+                :value="item.code">
+              </el-option>
+            </el-select>
+          </el-form-item>
+
+          <el-form-item label="产品模版" prop="productId">
+            <el-select
+              style="width:100%"
+              v-model="ruleForm.productId"
+              clearable
+              placeholder="请选择"
+              @change="handleProductIdChange"
+            >
+              <el-option
+                v-for="(item,index) in productOptions"
+                :key="item.productId + '' + index "
+                :label="`${item.prodTempName}`"
+                :value="item.productId">
+                <span>{{item.prodTempName}}</span>
+                <span class="fr">{{item.num}}件</span>
+              </el-option>
+            </el-select>
+          </el-form-item>
+
+          <div class="mb-5">
+            <span class="icon-sign">筛选结果</span>
+          </div>
+
+          <div class="screening-results">
+            共找到案件
+            <span class="color-origin">{{totalCount}}</span>
+            件，待分配
+            <span class="color-origin">
+              {{
+                undistributeTotalCount >= 0 ? undistributeTotalCount : ''
+              }}
+            </span>
+          </div>
+
+          <div>
+            <span class="icon-sign">初审分配</span>
+          </div>
+
+          <ul class="case-distribution">
+            <template v-for="(item,index) in ruleForm.allocationList">
+
+              <li :Key="index">
+                <el-form-item :label="item.userName"
+                  :prop="`allocationList.${index}.count`"
+                  :rules="[
+
+                    {pattern : /^[0-9]*$/, message : '格式有误' , trigger : 'change'},
+                  ]"
+                >
+                  <el-input
+                    :disabled="item.disabled === true ? true : false"
+                    @input.native="handleAuditedChange(item.count,index,item)"
+                    style="width:200px;"
+                    v-model="item.count" placeholder="请输入"
+                  /><span class="ml-10">件</span>
+                  （
+                  <span class="color-origin">{{item.unDealCount}}</span>
+                  件待审核）
+                </el-form-item>
+              </li>
+
+            </template>
+          </ul>
+
+          <div>
+            <span class="icon-sign">复审人</span>
+          </div>
+
+          <div class="screening-results">
+            <template v-if="ruleForm.allocationList[0].disabled === false">
+              <span>--</span>
+            </template>
+            <template v-else>
+              
+              <template>
+                <span>未指定</span>
+                <el-button @click="handleSet" class="ml-10" type="text">设置</el-button>
+              </template>
+            </template>
+          </div>
+
+        </el-form>
+      </div>
+
+      <span slot="footer" class="dialog-footer">
+        <el-button :disabled="submitDisabled" type="primary" @click="handleSubmit">确 定</el-button>
+        <el-button @click="handleClose">取 消</el-button>
+      </span>
+    </el-dialog>
+
+    <setDialog ref="setDialog"></setDialog>
+  </div>
+</template>
+
+<script>
+  import timeFrame from '@/components/timeFrame.vue'
+  import setDialog from './setDialog.vue'
+
+  export default {
+    components : {
+      timeFrame,
+      setDialog,
+    },
+    data() {
+      return {
+        dialogVisible : false,
+        // 提交按钮禁用状态
+        submitDisabled : false,
+
+        ruleForm : {
+          // 互金企业
+          merchantCode : '',
+          // 产品
+          productId : '',
+
+          // 运营人员 列表
+          allocationList : [
+            {
+              // 运营人员 姓名
+              userName : '啦啦啦方法',
+              // 运营人员 id
+              userId : '1',
+              // 已分配 数量
+              count : '',
+              // 待审核 数量
+              unDealCount : '',
+              // 当前是否 禁用
+              disabled : true,
+              // 保存案件分配 上次输入的数据
+              countOld : '',
+            },
+          ],
+        },
+        rules : {
+          merchantCode : [
+            {required : true , message : '请选择互金企业' , trigger : 'change'},
+          ],
+          productId : [
+            {required : true , message : '请选择产品' , trigger : 'change'},
+          ],
+        },
+
+        // 互金企业 options
+        merchantOptions : [
+          /* {merchantName : '张三' , code : '张三'} */
+        ],
+        // 产品 options
+        productOptions : [
+          /* {prodName : '产品' , prodCode : '产品'} */
+        ],
+
+        // 总案件数量
+        totalCount : '0',
+        // 待分配案件数量
+        undistributeTotalCount : '0',
+        // 待分配案件数量 - 暂存值
+        undistributeTotalCountStorage : '0',
+      }
+    },
+    mounted() {
+
+    },
+    methods : {
+      show() {
+        this.dialogVisible = true;
+        // 初始化 查询全部互金企业
+        this.remoteMethod('');
+        // 查询所有运营 人员
+        this.$http({
+          method : 'post',
+          url : '/preCaseLib/queryUserAndDistributeCaseInfo.htm',
+        }).then((res) => {
+          this.ruleForm.allocationList = res.result.map((v) => {
+            v.count = '';
+            v.disabled = true;
+            v.countOld = '';
+            return v;
+          });
+        });
+      },
+
+      // 更新筛选结果
+      updateResult() {
+        // 重置 人员待分配 数量
+        this.ruleForm.allocationList = this.ruleForm.allocationList.map((v) => {
+          v.count = '';
+          return v;
+        });
+        // 重置 总案件数量 和 待分配案件数量
+        this.totalCount = 0;
+        this.undistributeTotalCount = 0;
+        // 禁止 运营人员输入
+        this.ruleForm.allocationList = this.ruleForm.allocationList.map((v) => {
+          v.disabled = true;
+          return v;
+        });
+        // 如果没有 互金企业 或者 企业 不查询筛选结果
+        if(!this.ruleForm.merchantCode || !this.ruleForm.productId){
+          return;
+        }
+        this.$http({
+          method : 'post',
+          url : '/preCaseLib/queryUnDistributeCaseInfoByCondition.htm',
+          data : {
+            accountPeriodType : this.ruleForm.accountPeriodType,
+            merchantCode : this.ruleForm.merchantCode,
+            productId : this.ruleForm.productId,
+            pushEndDate : this.ruleForm.pushEndDate,
+            pushStartDate : this.ruleForm.pushStartDate,
+          },
+        }).then((res) => {
+          // 需求修改 初始化时 总案件数 一定等于 待分配件数 
+          this.totalCount = res.result.undistributeTotalCount;
+          this.undistributeTotalCount = res.result.undistributeTotalCount;
+          this.undistributeTotalCountStorage = res.result.undistributeTotalCount;
+          // 开启 运营人员输入
+          this.ruleForm.allocationList = this.ruleForm.allocationList.map((v) => {
+            v.disabled = false;
+            return v;
+          });
+        });
+      },
+
+      // 互金企业搜索
+      remoteMethod(query) {
+        this.$http({
+          method : 'post',
+          url : '/merchant/queryPreMerchantName.htm',
+          data : {
+            keyWords : query,
+          },
+        }).then((res) => {
+          this.merchantOptions = res.result;
+        });
+      },
+      // 互金企业change
+      handleMerchantCodeChange(val) {
+        // 清空 产品options ro 保存的产品 id
+        this.productOptions = [];
+        this.ruleForm.productId = '';
+
+        if(!val) {
+          this.updateResult();
+          this.$refs.ruleForm.validateField(`productId`);
+          return;
+        }
+        // 根据互金企业 查询 产品
+        this.$http({
+          method : 'post',
+          url : '/merchant/selectMerchantProductByMerchantCode.htm',
+          data : {
+            merchantCode : val,
+          },
+        }).then((res) => {
+          this.productOptions = res.result;
+          this.updateResult();
+        });
+      },
+      // 产品 change
+      handleProductIdChange(val) {
+        this.updateResult();
+      },
+      // 案件分配 change
+      handleAuditedChange(val,index,item) {
+        // 通过校验
+        if(/^[0-9]*$/.test(val)){
+          // 判断用户输入第一位 不能为0
+          if(val[0] == '0'){
+            val = '';
+            setTimeout(() => {
+              this.ruleForm.allocationList[index].count = '';
+            },0);
+          // 如果所填 大于 总数量 默认最大数量值
+          }else if(val > (+this.undistributeTotalCount) + (+item.countOld) ) {
+            // 如果当前 可分配为 0 ， 重置为 之前数据
+            if(this.undistributeTotalCount == '0') {
+              val = item.countOld;
+              setTimeout(() => {
+                this.ruleForm.allocationList[index].count = val;
+              },0);
+
+            }else{
+              // 设为最大
+              let max = (+this.undistributeTotalCount) + (+item.countOld);
+              this.ruleForm.allocationList[index].count = max;
+              val = max;
+            }
+          // 正常处理数据
+          }else{
+            if(+val > +item.countOld){
+              // 如果当前 可分配为 0 ， 重置为 之前数据
+              if(this.undistributeTotalCount == '0') {
+                val = item.countOld;
+                setTimeout(() => {
+                  this.ruleForm.allocationList[index].count = val;
+                },0);
+              }
+            }
+          }
+
+          // 更新 待分配数量
+          setTimeout(() => {
+            // 当前案件分配 已分配数量
+            let countMax = 0;
+            this.ruleForm.allocationList.map((v) => {
+              countMax = countMax + (+v.count);
+            });
+            this.undistributeTotalCount = (+this.undistributeTotalCountStorage) - countMax;
+          },0);
+        // 格式有误
+        }else{
+          setTimeout(() => {
+            // 重置当前输入 为空
+            this.ruleForm.allocationList[index].count = '';
+            // 重新计算 待分配数量
+            let countMax = 0;
+            this.ruleForm.allocationList.map((v) => {
+              countMax = countMax + (+v.count);
+            });
+            this.undistributeTotalCount = (+this.undistributeTotalCountStorage) - countMax;
+          },0);
+        }
+        this.ruleForm.allocationList[index].countOld = val;
+      },
+      // 点击 设置
+      handleSet() {
+        this.$refs.setDialog.show();
+      },
+
+      // 关闭浮层
+      handleClose() {
+        this.dialogVisible = false;
+        // 取消按钮禁用
+        setTimeout(() => {
+          this.submitDisabled = false;
+          this.$refs.ruleForm.resetFields();
+        },500);
+
+        // 禁止 运营人员输入
+        this.ruleForm.allocationList = this.ruleForm.allocationList.map((v) => {
+          v.disabled = true;
+          return v;
+        });
+        // 重置 总案件数量 和 待分配案件数量
+        this.totalCount = 0;
+        this.undistributeTotalCount = 0;
+        // 清空下拉框数据
+        this.merchantOptions = [];
+        this.productOptions = [];
+
+      },
+      // 点击提交
+      handleSubmit(submitType) {
+        this.$refs.ruleForm.validate((valid) => {
+          if(valid) {
+            // 校验待分配案件 是否变动
+            if(this.undistributeTotalCount === this.undistributeTotalCountStorage){
+              this.$message.warning('请至少分配一件案件');
+              return;
+            }
+
+            let data = {...this.ruleForm};
+            // 处理数据
+            data.allocationList = (data.allocationList.map((v) => {
+              return {
+                count : v.count,
+                userId : v.userId,
+                userName : v.userName,
+              }
+            }));
+            this.submitDisabled = true;
+            this.$http({
+              method : 'post',
+              url : '/preCaseLib/distributeCaseByDistributeCaseQuery.htm',
+              data : data,
+              mheaders : true,
+            }).then((res) => {
+              this.$message.success('分配成功');
+              this.handleClose();
+              this.$emit('successCBK');
+            }).catch(() => {
+              this.submitDisabled = false;
+            });
+          }
+        });
+      },
+    },
+  }
+</script>
+
+<style lang="scss">
+
+.review-case-list-dialog{
+  .color-origin{
+    color: #EEAB31;
+  }
+  .time-frame{
+    .el-form-item{
+      display: inline-block;
+    }
+    .m-span{
+      margin-top: 0;
+      margin-left: 5px;
+    }
+  }
+
+  .screening-results{
+    padding-left: 30px;
+    margin-bottom: 10px;
+  }
+  .case-distribution{
+    .el-form-item.is-required .el-form-item__label:before{
+      display: none;
+    }
+  }
+}
+
+</style>
