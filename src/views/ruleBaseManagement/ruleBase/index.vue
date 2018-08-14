ec73a1e959fccd3117dca081cb23fca786358cb6
zengwei <18279047775@163.com>
2018-08-14 19:49:44 +0800

    规则库

--- src/views/ruleBaseManagement/ruleBase/index.vue
+++ src/views/ruleBaseManagement/ruleBase/index.vue
@@ -1,7 +1,30 @@
 <template>
   <div class="rule-base">
-    <div class="m-bar">
-      <m-progress></m-progress>
+    <!-- <div class="m-bar">
+      <m-progress :width="progress" :height="20">执行中</m-progress>
+    </div> -->
+    <div class="title-box">
+      <div>
+        <span class="m-title">规则库</span>
+        <div class="fr">
+          <el-button type="primary" class="mt-20">执行规则</el-button>
+        </div>
+      </div>
+    </div>
+    <div class="content-box">
+      <div class="fl m-left">
+        <el-tree 
+          :data="treeData" 
+          :props="{children : 'children' , label : 'label'}"
+          :highlight-current="true"
+          :expand-on-click-node="false"
+          @node-click="handleNodeClick"
+        >
+        </el-tree>
+      </div>
+      <div class="fl m-right">
+
+      </div>
     </div>
   </div>
 </template>
@@ -12,8 +35,45 @@
     components : {
       'm-progress' : progress,
     },
+    data() {
+      return {
+        progress : 80,
+        treeData : [
+          {
+            label : '公共规则',
+            children : [
+              {
+                label : 'P2P现金贷业务',
+                children : [
+                  {
+                    label : '奇速贷',
+                    children : [
+                      {
+                        label : '奇速贷-1001',
+                        id : 1,
+                      },
+                      {
+                        label : '奇速贷-1002',
+                      },
+                      {
+                        label : '奇速贷-1003',
+                      },
+                    ],
+                  }
+                ],
+              }
+            ],
+          }
+        ],
+      }
+    },
     mounted() {
 
+    },
+    methods : {
+      handleNodeClick(row) {
+        console.log(row);
+      },
     },
   }
 </script>
@@ -23,10 +83,34 @@
 .rule-base{
   height: 100%;
   overflow: auto;
-  .m-bar{
-    height: 40px;
-    .progress-bar{
-      width: 42px;
+  background-color: #F1F2F7;
+  .title-box{
+    height: 75px;
+    background-color: #fff;
+    >div{
+      width: 1200px;
+      margin: 0 auto;
+    }
+    .m-title{
+      font-size: 30px;
+      color: #0f357f;
+      display: inline-block;
+      margin-top: 21px;
+    }
+  }
+  .content-box{
+    width: 1200px;
+    margin: 0 auto;
+    margin-top: 10px;
+    .m-left{
+      width: 300px;
+    }
+    .m-right{
+      width: 890px;
+      margin-left: 10px;
+      background-color: #fff;
+
+      height: 300px;
     }
   }
 }
--- src/views/ruleBaseManagement/ruleBase/modules/progress.vue
+++ src/views/ruleBaseManagement/ruleBase/modules/progress.vue
@@ -1,13 +1,24 @@
 <template>
-  <div class="m-progress">
-    <div class="progress-bar progress-bar-striped active"></div>
+  <div class="m-progress" :style="{height : height + 'px'}">
+    <div :style="{width : width + '%'}" class="progress-bar progress-bar-striped active progress-color">
+    </div>
+    <div class="slot" :style="{'line-height' : height - 2 + 'px'}">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
     props : {
-      
+      width : {
+        type : Number,
+        default : 0,
+      },
+      height : {
+        type : Number,
+        default : 30,
+      },
     },
   }
 </script>
@@ -15,18 +26,29 @@
 <style lang="scss" scoped>
 
 .m-progress{
-  width: 30px;
-  height: 40px;
+  width: 100%;
+  position: relative;
+  box-sizing: border-box;
+  border: 1px solid #8E71C7;
+  .slot{
+    position: absolute;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    text-align: center;
+  }
   // 进度条样式
+  .progress-color{
+    background-color: #8E71C7;
+  }
   .progress-bar{
     float: left;
-    width: 20px;
     height: 100%;
     font-size: 12px;
     line-height: 20px;
     color: #fff;
     text-align: center;
-    background-color: #337ab7;
     -webkit-box-shadow: inset 0 -1px 0 rgba(0,0,0,.15);
     box-shadow: inset 0 -1px 0 rgba(0,0,0,.15);
     -webkit-transition: width .6s ease;
