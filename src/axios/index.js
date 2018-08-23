3b097cb7f938ef42640259b7630ee664c7ad2cb5
zengwei <18279047775@163.com>
2018-08-23 14:38:00 +0800

    axios 超时时间修改

--- src/axios/index.js
+++ src/axios/index.js
@@ -10,7 +10,7 @@ import host from './host'
 
 
 
-axios.defaults.timeout = 10000;
+axios.defaults.timeout = 60000;
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 // axios.defaults.headers.common['Access-Control-Request-Headers'] = null
 // axios.defaults.headers.common['Access-Control-Request-Method'] = null
