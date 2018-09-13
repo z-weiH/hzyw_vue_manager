68f1cf602cc07046d6cc7c8dfbca784207dca41c
zengwei <18279047775@163.com>
2018-09-13 15:41:48 +0800

    级联 clearable

--- src/views/execManage/judgeRegisterInfo/modules/examineDialog.vue
+++ src/views/execManage/judgeRegisterInfo/modules/examineDialog.vue
@@ -13,6 +13,7 @@
           <el-form-item label=" " prop="accountPeriodType">
             <el-cascader
               :options="cityOptions"
+              clearable
               v-model="ruleForm.selectedOptions"
               @change="handleRegion"
               placeholder="地区"
