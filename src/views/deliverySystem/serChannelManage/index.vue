152c9aceddfa2a290bafe23ffa5db1fcf4a420f9
zengwei <18279047775@163.com>
2018-10-10 18:03:39 +0800

    渠道管理

--- src/views/deliverySystem/serChannelManage/index.vue
+++ src/views/deliverySystem/serChannelManage/index.vue
@@ -1,15 +1,148 @@
 <template>
-  <div>
+	<div class="ser-channel-manage">
+    <div class="wsbodyhead">
+      <a>所在位置</a>
+      <a href="javascript:;" class="aside_tit">渠道管理</a>
+    </div>
+		<div class="item-search">
+      <el-form :inline="true" ref="ruleForm" :model="ruleForm" label-width="0px">
+        <el-form-item label=" " prop="keyWords">
+          <el-input v-model.trim="ruleForm.keyWords" placeholder="请输入渠道名称"></el-input>
+        </el-form-item>
+        <el-form-item label=" " prop="keyWords2">
+          <el-input v-model.trim="ruleForm.keyWords2" placeholder="请输入渠道编码"></el-input>
+        </el-form-item>
+        <el-button @click="handleSearch" type="warning">查询</el-button>
 
-  </div>
+        <div class="fr">
+          <el-button type="primary">新增</el-button>
+        </div>
+      </el-form>
+    </div>
+
+		<div class="item-table">
+      <el-table
+        :data="tableData"
+        border
+      >
+        <el-table-column prop="date" label="序号" width="50px">
+          <template slot-scope="scope">
+            {{scope.$index + 1}}
+          </template>
+        </el-table-column>
+				<el-table-column prop="respondents" label="渠道编码"></el-table-column>
+        <el-table-column prop="respondents" label="渠道名称"></el-table-column>
+        <el-table-column prop="respondents" label="渠道号码"></el-table-column>
+        <el-table-column prop="respondents" label="创建时间"></el-table-column>
+        <el-table-column prop="platName" label="操作" align="center">
+          <template slot-scope="scope">
+            <el-button type="text">编辑</el-button>
+            <el-button type="text">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- 分页 -->
+      <el-pagination
+        class="mt-10 mb-10"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="currentPage"
+        :page-sizes="[10, 20, 30, 40]"
+        :page-size="10"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total">
+      </el-pagination>
+
+    </div>
+	</div>
 </template>
 
 <script>
-  export default {
-    
-  }
+	import timeFrame from '@/components/timeFrame.vue'
+	export default {
+		components : {
+			timeFrame,
+		},
+		data() {
+			return {
+				ruleForm : {
+					// 关键字
+					keyWords : '',
+				},
+				rules : {},
+
+				// 表格数据
+        tableData : [],
+        // 数据总数
+        total : 11,
+        // 当前页数
+        currentPage : 1,
+        // 每页数量
+				pageSize : 10,
+				
+			}
+    },
+    mounted() {
+      this.initTableList();
+    },
+		methods : {
+			// 点击搜索
+			handleSearch() {
+				this.currentPage = 1;
+        this.initTableList();
+			},
+
+			// 表格相关 start
+
+      // 初始化 表格数据
+      initTableList() {
+        this.$http({
+          url : '/preCaseLib/queryCaseListByCondition.htm',
+          method : 'post',
+          data : {
+            pageSize : this.pageSize,
+            currentNum : this.currentPage,
+
+            ...this.ruleForm,
+          },
+        }).then((res) => {
+          this.total = res.result.count;
+          this.tableData = res.result.list;
+        });
+      },
+      // 页数 change
+      handleSizeChange(val) {
+        this.pageSize = val;
+        this.currentPage = 1;
+        this.initTableList();
+      },
+      // 分页 change
+      handleCurrentChange(val) {
+        this.currentPage = val; 
+        this.initTableList();
+      },
+
+      // 表格相关 end
+		},
+	}
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+
+.ser-channel-manage{
+	
+}
+
+</style>
+
+<style lang="scss">
+
+.demo-box{
+  .item-search{
+    .el-form-item{
+      margin-bottom: 0;
+    }
+  }
+}
 
 </style>
