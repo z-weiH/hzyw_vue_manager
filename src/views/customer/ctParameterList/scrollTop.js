5b5606d6ae235eec36b9e1b0e8f8c7a5655978eb
zengwei <18279047775@163.com>
2018-11-02 16:59:28 +0800

    style

--- src/views/customer/ctParameterList/scrollTop.js
+++ src/views/customer/ctParameterList/scrollTop.js
@@ -1,13 +1,13 @@
 import { throttle } from '@/assets/js/tool'
-let fn;
+let scrollFn;
 
 export default {
   init(callback) {
-    let scrollFn = throttle((e,callback) => {
+    scrollFn = throttle(() => {
       // 高亮判断的位置元素
       let arr = Array.map(document.querySelectorAll('.scroll-top-item'),v => v.offsetTop);
       // 当前滚动条距离 顶部的距离
-      let top = e.target.scrollTop;
+      let top = document.querySelector('.main').scrollTop;
       // 判断当前滚动距离 距离哪个元素范围
       for(let index = 0 ; index < arr.length ; index ++) {
         let current = arr[index];
@@ -18,14 +18,12 @@ export default {
         }
       };
     },60);
-    fn = (e) => {
-      scrollFn(e,callback);
-    };
-    document.querySelector('.main').addEventListener('scroll',fn,false);
+    document.querySelector('.main').addEventListener('scroll',scrollFn,false);
+    scrollFn();
   },
 
   unbind() {
-    document.querySelector('.main').removeEventListener('scroll',fn);
+    document.querySelector('.main').removeEventListener('scroll',scrollFn);
   },
 
 }
