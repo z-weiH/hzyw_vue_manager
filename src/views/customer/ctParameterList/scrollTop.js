aa54749aa04647f5ac555c035235bf04f43a4567
zengwei <18279047775@163.com>
2018-11-02 15:33:07 +0800

    滚动高亮

---
+++ src/views/customer/ctParameterList/scrollTop.js
@@ -0,0 +1,31 @@
+import { throttle } from '@/assets/js/tool'
+let fn;
+
+export default {
+  init(callback) {
+    let scrollFn = throttle((e,callback) => {
+      // 高亮判断的位置元素
+      let arr = Array.map(document.querySelectorAll('.scroll-top-item'),v => v.offsetTop);
+      // 当前滚动条距离 顶部的距离
+      let top = e.target.scrollTop;
+      // 判断当前滚动距离 距离哪个元素范围
+      for(let index = 0 ; index < arr.length ; index ++) {
+        let current = arr[index];
+        let next = arr[index + 1];
+        // （当前滚动距离大于等于元素距离顶部位置 且 当前滚动距离小于下一个元素位置） || （如果当前是最后一个元素，滚动距离大于自身）
+        if( (top >= current && top < next) || (!next && top >= current) ) {
+          callback(index,current,next);
+        }
+      };
+    },60);
+    fn = (e) => {
+      scrollFn(e,callback);
+    };
+    document.querySelector('.main').addEventListener('scroll',fn,false);
+  },
+
+  unbind() {
+    document.querySelector('.main').removeEventListener('scroll',fn);
+  },
+
+}
