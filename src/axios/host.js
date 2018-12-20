ec13e87d679225ecb50b87ad61fb1da479317ef2
zengwei <18279047775@163.com>
2018-12-20 14:53:38 +0800

    host 修改

--- src/axios/host.js
+++ src/axios/host.js
@@ -10,7 +10,7 @@ let host = {
   // target: 'http://118.31.39.65:7030',   // 测试环境
   // target: 'http://192.168.30.64:3000',   // 本地node
   // target: 'http://hzapi.arbexpress.cn',
-  target: '/manage',
+  target: '/manager',
 };
 
 
