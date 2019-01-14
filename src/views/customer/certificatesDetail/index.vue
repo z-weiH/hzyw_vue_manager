e738c37fb50d565c1620867c313f681f93d4f3c1
zengwei <18279047775@163.com>
2019-01-14 18:53:43 +0800

    仲券充值总数 明细主要逻辑

--- src/views/customer/certificatesDetail/index.vue
+++ src/views/customer/certificatesDetail/index.vue
@@ -28,6 +28,58 @@
       </div>
     </div>
 
+    <div class="statistics-box">
+      <el-row :gutter="10">
+        <el-col :span="8">
+          <div class="m-grid-content">
+            <p>充值仲券总数（张）</p>
+            <div class="grid-num grid-num-origin">{{statistics.rechargeTotal}}</div>
+          </div>
+        </el-col>
+        <el-col :span="8">
+          <div class="m-grid-content">
+            <p>赠券总数（张）</p>
+            <div class="grid-num grid-num-origin">{{statistics.giftTotal}}</div>
+          </div>
+        </el-col>
+        <el-col :span="8">
+          <div class="m-grid-content">
+            <p>退券总数（张）</p>
+            <div class="grid-num grid-num-origin">{{statistics.refundTotal}}</div>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+
+    <div class="item-search">
+      <el-form :inline="true" ref="searchForm" :model="searchForm" label-width="0px">
+        <el-form-item label=" " prop="clientCode">
+          <el-select filterable clearable v-model="searchForm.clientCode" placeholder="请选择客户">
+            <el-option label="全部" value=""></el-option>
+            <el-option :label="item.merchantName" :value="item.code" v-for="(item,index) in clientOptions" :key="index"></el-option>
+          </el-select>
+        </el-form-item>
+
+        <el-form-item label=" " prop="hasRecharge">
+          <el-checkbox v-model="searchForm.hasRecharge">有充值记录</el-checkbox>
+        </el-form-item>
+
+        <el-form-item label=" " prop="hasGift">
+          <el-checkbox v-model="searchForm.hasGift">有赠券记录</el-checkbox>
+        </el-form-item>
+
+        <el-form-item label=" " prop="hasRefund">
+          <el-checkbox v-model="searchForm.hasRefund">有退券记录</el-checkbox>
+        </el-form-item>
+
+        <el-button @click="handleSearch" type="warning">查询</el-button>
+
+        <div class="fr">
+          <el-button @click="handleExportFile" type="primary">导出</el-button>
+        </div>
+      </el-form>
+    </div>
+
     <div class="item-table">
       <el-table
         :data="tableData"
@@ -38,12 +90,50 @@
             {{scope.$index + 1}}
           </template>
         </el-table-column>
-				<el-table-column prop="respondents" label="被申请人"></el-table-column>
-        <el-table-column prop="platName" label="互金企业" align="center">
+				<el-table-column prop="clientName" label="客户名称"></el-table-column>
+        <el-table-column prop="finalTicket" label="期末仲券数">
+          <template slot="header" slot-scope="scope">
+            <span :key="scope.$index">
+              期末仲券数
+              <el-tooltip class="item" effect="dark" placement="bottom">
+                <i class="el-icon-info ml-10"></i>
+                <div slot="content">
+                  期末仲券数意思为：所选时间范
+                  <br />围内，该客户账户最新的可用仲
+                  <br />券数，如：选择查看1月1号到1
+                  <br />月30号时间范围，那么此项将显
+                  <br />示1月30号0点时，该账户的可
+                  <br />用仲券数量
+                </div>
+              </el-tooltip>
+            </span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="finalTicketGift" label="期末赠券数">
+          <template slot="header" slot-scope="scope">
+            <span :key="scope.$index">
+              期末赠券数
+              <el-tooltip class="item" effect="dark" placement="bottom">
+                <i class="el-icon-info ml-10"></i>
+                <div slot="content">
+                  期末赠券数意思为：所选时间范
+                  <br />围内，该客户账户最新的可用赠
+                  <br />券数量，如：选择查看1月1号到
+                  <br />1月30号时间范围，那么此项将
+                  <br />显示1月30号0点时，该账户可
+                  <br />用赠券数量
+                </div>
+              </el-tooltip>
+            </span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="ticketValue" label="单张券面值（元）"></el-table-column>
+        <el-table-column prop="periodTicketRecharge" label="期间充值仲券"></el-table-column>
+        <el-table-column prop="periodTicketGift" label="期间赠券"></el-table-column>
+        <el-table-column prop="periodTicketRefund" label="期间退券"></el-table-column>
+        <el-table-column label="操作">
           <template slot-scope="scope">
-            <el-tooltip :content="scope.row.platName" placement="top-start">
-              <span class="ellipsis" style="max-width:130px;">{{scope.row.platName}}</span>
-            </el-tooltip>
+            <el-button @click="handleDetail(scope)" type="text">查看详情</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -60,18 +150,24 @@
       </el-pagination>
 
     </div>
+
+    <detailDialog ref="detailDialog"></detailDialog>
   </div>
 </template>
 
 <script>
   import timeFrame from '@/components/timeFrame.vue'
+  import exportFile from '@/assets/js/exportFile'
+  import detailDialog from './modules/detailDialog.vue'
   export default {
     components : {
       timeFrame,
+      detailDialog,
     },
     data() {
       let time = this.$moment().format('YYYY-MM-DD');
       return {
+        // 用于统计搜索
         ruleForm : {
           // 开始日期
           startDate : time,
@@ -80,30 +176,99 @@
         },
         startDateText : time,
         endDateText : time,
+        // 统计
+        statistics : {
+          giftTotal : '1', // 赠券总数（张）
+          rechargeTotal : '2', // 充值仲券总数（张）
+          refundTotal : '3', // 退券总数（张）
+        },
+        // 用于表格搜索
+        searchForm : {
+          // 客户
+          clientCode : '',
+          // 赠券记录 0否 1是
+          hasGift : '',
+          // 充值记录 0否 1是
+          hasRecharge : '',
+          // 退券记录 0否 1是
+          hasRefund : '',
+        },
 
         // 表格数据
-        tableData : [],
+        tableData : [{}],
         // 数据总数
         total : 11,
         // 当前页数
         currentPage : 1,
         // 每页数量
-				pageSize : 10,
+        pageSize : 10,
+        
+        // 客户options
+        clientOptions : [],
       }
     },
     mounted() {
-      console.log(this);
+      this.initClient();
+      this.initStatistics();
+      this.initTableList();
     },
     methods : {
+      // 获取客户 options
+      initClient() {
+        return this.$http({
+          method : 'post',
+          url : '/merchant/queryMerchants.htm',
+        }).then((res) => {
+          this.clientOptions = res.result.list;
+        });
+      },
+      // 获取统计
+      initStatistics() {
+        return this.$http({
+          method : 'post',
+          url : '/account/queryRechargeTotal.htm',
+          data : {
+            bizType : 1,
+            ...this.ruleForm,
+          },
+        }).then((res) => {
+          this.statistics = Object.assign(this.statistics,res.result);
+        });
+      },
       // 时间搜索
       handleTimeSearch() {
         if(!this.ruleForm.startDate && !this.ruleForm.endDate) {
           return this.$message.warning('请至少选择一个时间');
         }
+        this.initStatistics().then(() => {
+          this.startDateText = this.ruleForm.startDate;
+          this.endDateText = this.ruleForm.endDate;
+        });
+      },
+      // 点击导出 
+      handleExportFile() {
+        exportFile({
+          url : '/account/queryTicketRechargeExport.htm',
+          data : {
+            ...this.ruleForm,
+            clientCode : this.searchForm.clientCode,
+            hasGift : this.searchForm.hasGift ? 1 : 0,
+            hasRecharge : this.searchForm.hasRecharge ? 1 : 0,
+            hasRefund : this.searchForm.hasRefund ? 1 : 0,
+          },
+        });
       },
       // 点击返回
       handleGoBack() {
         this.$router.push('balanceQuery');
+      },
+      // 点击查询
+      handleSearch() {
+        this.initTableList();
+      },
+      // 表格详情
+      handleDetail(row) {
+        this.$refs.detailDialog.show(row);
       },
 
 
@@ -112,13 +277,17 @@
       // 初始化 表格数据
       initTableList() {
         this.$http({
-          url : '/preCaseLib/queryCaseListByCondition.htm',
+          url : '/account/queryTicketRechargeList.htm',
           method : 'post',
           data : {
             pageSize : this.pageSize,
             currentNum : this.currentPage,
 
             ...this.ruleForm,
+            clientCode : this.searchForm.clientCode,
+            hasGift : this.searchForm.hasGift ? 1 : 0,
+            hasRecharge : this.searchForm.hasRecharge ? 1 : 0,
+            hasRefund : this.searchForm.hasRefund ? 1 : 0,
           },
         }).then((res) => {
           this.total = res.result.count;
@@ -159,6 +328,40 @@
     .el-input__inner{
       height: 28px;
       line-height: 28px;
+    }
+  }
+
+  .statistics-box{
+    margin-top: 10px;
+    margin-bottom: 10px;
+    .m-grid-content{
+      background-color: #fff;
+      height: 90px;
+      padding: 12px 12px 5px 20px;
+      box-sizing: border-box;
+
+      .grid-num{
+        font-size: 22px;
+        font-weight: bold;
+        padding-top: 12px;
+        padding-bottom: 12px;
+        text-align: center;
+      }
+      .grid-num-origin{
+        color: #FF9933;
+      }
+      .grid-num-green{
+        color: #66CC66;
+      }
+      .grid-detail{
+        text-align: right;
+        margin-top: -20px;
+      }
+    }
+  }
+  .item-search{
+    .el-form-item{
+      margin-bottom: 0;
     }
   }
 }
---
+++ src/views/customer/certificatesDetail/modules/detailDialog.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="certificates-detail-detail-dialog">
+    <el-dialog
+      title="详情"
+      :visible.sync="dialogVisible"
+      width="580px"
+      @close="handleClose"
+			ref="dialog"
+    >
+      <div class="m-conetnt">
+
+      </div>
+
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        dialogVisible : false,
+      }
+    },
+    mounted() {
+
+    },
+    methods : {
+      show(type,data) {
+				this.dialogVisible = true;
+				// dialog 返回顶部
+        this.$nextTick(() => {
+          this.$refs.dialog.$el.scrollTop = 0;
+        });
+
+        this.$nextTick(() => {
+          // 处理逻辑 写在nextTick中 ， 防止dialog没有加载数据问题
+        });
+      },
+
+      // 关闭浮层
+      handleClose() {
+        this.dialogVisible = false;
+        
+        setTimeout(() => {
+				},500);
+				
+      },
+    },
+  }
+</script>
+
+<style scoped lang="scss">
+
+.certificates-detail-detail-dialog{
+
+}
+
+</style>
