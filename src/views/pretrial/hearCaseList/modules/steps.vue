1d92c0834de1ec4174d7a76f8f1068bb08245bc9
wanhuanchang <1109923832@qq.com>
2019-03-08 19:51:20 +0800

    .

--- src/views/pretrial/hearCaseList/modules/steps.vue
+++ src/views/pretrial/hearCaseList/modules/steps.vue
@@ -290,7 +290,7 @@
                   <span class="label">审核意见</span>
                   <span class="value">
                     <ul>
-                       <li v-for="(reason,idx) in currentObj.logObj.totalReasonList">{{`${idx + 1}.${reason.reasonMsg}`}}</li>
+                       <li v-for="(reason,idx) in currentObj.logObj.totalReasonList">{{`${idx + 1}.${reason}`}}</li>
                         <li v-if="!currentObj.logObj.totalReasonList || currentObj.logObj.totalReasonList.length === 0">通过</li>
                     </ul>
                   </span>
@@ -343,7 +343,7 @@
                   <span class="label">审核意见</span>
                   <span class="value">
                     <ul>
-                       <li v-for="(reason,idx) in currentObj.logObj.totalReasonList">{{`${idx + 1}.${reason.reasonMsg}`}}</li>
+                       <li v-for="(reason,idx) in currentObj.logObj.totalReasonList">{{`${idx + 1}.${reason}`}}</li>
                         <li v-if="!currentObj.logObj.totalReasonList || currentObj.logObj.totalReasonList.length === 0">通过</li>
                     </ul>
                 </span>
