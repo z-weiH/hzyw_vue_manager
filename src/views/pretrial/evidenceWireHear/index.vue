72cbb12c821a2e33f4ad001ef5c8672a9a0dc4ec
zengwei <18279047775@163.com>
2019-06-19 11:01:04 +0800

    更换 pdf url

--- src/views/pretrial/evidenceWireHear/index.vue
+++ src/views/pretrial/evidenceWireHear/index.vue
@@ -83,10 +83,10 @@
               <div class="applybook_content of-hidden">
                 <div class="article_left fl">
                   <!--<pdf :src="evidence.applicationUrl"></pdf>-->
-                  <iframe  :src="applicationUrl" width="100%" height="100%" frameborder="0" scrolling="yes"></iframe>
+                  <iframe  :src="'/static/pdfjs/web/viewer.html?file=' + applicationUrl" width="100%" height="100%" frameborder="0" scrolling="yes"></iframe>
                 </div>
                 <div  ref="evidenceWarper" class="article_right fr">
-                  <iframe ref="evidence" v-if="checkPdf(currentUrl)"  :src="currentUrl.replace(/http:|https:/g,'')" width="100%" height="100%" frameborder="0" scrolling="yes"></iframe>
+                  <iframe ref="evidence" v-if="checkPdf(currentUrl)"  :src="'/static/pdfjs/web/viewer.html?file=' + currentUrl.replace(/http:|https:/g,'') + '?'" width="100%" height="100%" frameborder="0" scrolling="yes"></iframe>
                   <div ref="imgEvi" style="overflow: auto;width:100%;height:100%;" v-else><img style="cursor: move;position: relative;width:100%;" :src="currentUrl" alt=""></div>
                 </div>
               </div>
